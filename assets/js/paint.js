13c02eeacc7dfc6589c93bcabb70188b5d2e7a0d
Zeth Thomas <zeththomas89@gmail.com>
2023-12-18 19:24:45 -0500

    jacksPaint

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,18 +1,20 @@
 function configureListeners() {
-    let images = // select img elements  
+    let images = document.querySelectorAll('img'); 
 
 
      for (var i = 0; i < images.length; i++) {        
-        // iterate over images and add mouseover event listeners      
+        images[i].addEventListener('mouseover', addOpacity);
+        images[i].addEventListener('mouseout', removeOpacity);     
     } 
 }
 
 function addOpacity(event) {
-    // add appropriate CSS class
+    event.target.classList.add('dim');
     getProductInfo(event.target.id);     
 }
 
 function removeOpacity(event) {
+    event.target.classList.remove('dim');
      //remove appropriate CSS class
 
     let element = document.getElementById('color-price');
@@ -29,43 +31,70 @@ function getProductInfo(paintColor) {
     let colorName;  
     
     switch (paintColor) {
-        case 'pn1':           
+        case 'pn1': 
+            price = 14.99
+            colorName = 'Green';      
             // set variables for price and color name and invoke a function to update the price     
             break;           
         case 'pn2':
+            price = 11.14
+            colorName = 'Brown';
             // set variables for price and color name and invoke a function to update the price    
             break;            
         case 'pn3':
+            price = 22.99
+            colorName = 'Blue';
             // set variables for price and color name and invoke a function to update the price  
             break;   
         case 'pn4':
+            price = 4.99
+            colorName = 'Black';
             // set variables for price and color name and invoke a function to update the price  
             break;   
         case 'pn5':
+            price = 8.22
+            colorName = 'Cyan';
             // set variables for price and color name and invoke a function to update the price       
             break;   
         case 'pn6':
+            price = 11.99
+            colorName = 'Purple';
             // set variables for price and color name and invoke a function to update the price        
             break;   
         case 'pn7':
+            price = 13.42
+            colorName = 'Red';
             // set variables for price and color name and invoke a function to update the price 
             break;   
         case 'pn8':
+            price = 21.98
+            colorName = 'White';
             // set variables for price and color name and invoke a function to update the price   
             break;   
         case 'pn9':
+            price = 14.99
+            colorName = 'Yellow';
             // set variables for price and color name and invoke a function to update the price 
             break;   
-          default:              
+          default:  
+          console.log('Invalid paint color selected');
+          return;            
     }
+    
+    updatePrice(colorName, price)
+
+    }
+
 
     function updatePrice(colorName, price)
     {       
-        let colorPrice = // select element with corresponding id
-        // display price
+        let colorPrice = document.getElementById('color-price');
+        colorPrice.textContent = 'Price: $' + price;
+   
         
-        let color = // select element with corresponding id
-        //display color name
+        let color = document.getElementById('color-name');
+        color.textContent = 'Color Name: ' + colorName;
+  
     }
     
-}
+
